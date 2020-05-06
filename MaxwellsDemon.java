43d74a83eadd806c1adee2bb1c79580b6e6a8272
alexanderwchen <calex34567@gmail.com>
2020-05-06 03:39:17 -0400

    Remove MainPanel and Particle

--- MaxwellsDemon.java
+++ MaxwellsDemon.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MaxwellsDemon extends JFrame implements ActionListener{
+public class MaxwellsDemon extends JFrame implements ActionListener {
 
     double deltat = .1; //  in seconds
     Timer timer;
@@ -18,12 +18,12 @@ public class MaxwellsDemon extends JFrame implements ActionListener{
     JPanel playingArea;
     JPanel temperatureArea;
     JPanel bottomArea;
-    JPanel mainArea;
+    MainPanel mainArea;
 
     JButton openDoorButton;
     JButton resetButton;
 
-    public static void main( String[] args ){
+    public static void main(String[] args) {
         System.out.println("Start");
         new MaxwellsDemon();
     }
@@ -53,84 +53,59 @@ public class MaxwellsDemon extends JFrame implements ActionListener{
 
         //top - temperatures
         temperatureArea = new JPanel();
-        temperatureArea.setLayout( new GridLayout(1,2, 50, 0 ));
+        temperatureArea.setLayout(new GridLayout(1, 2, 50, 0));
         JLabel temperatureLeft = new JLabel("TEMPERATURE1: $$", JLabel.CENTER);
         temperatureLeft.setFont(new Font("Bebas Neue", Font.PLAIN, 24));
         JLabel temperatureRight = new JLabel("TEMPERATURE2: $$", JLabel.CENTER);
         temperatureRight.setFont(new Font("Bebas Neue", Font.PLAIN, 24));
         temperatureArea.add(temperatureLeft);
         temperatureArea.add(temperatureRight);
-        playingArea.add(temperatureArea,BorderLayout.NORTH);
+        playingArea.add(temperatureArea, BorderLayout.NORTH);
 
         //top - temperatures
-        mainArea = new JPanel();
-        mainArea.setPreferredSize(new Dimension(maxx, maxy));
-        mainArea.setBackground(Color.WHITE);
+        mainArea = new MainPanel();
 
-        playingArea.add(mainArea,BorderLayout.CENTER);
+        playingArea.add(mainArea, BorderLayout.CENTER);
 
 
-        add(playingArea,BorderLayout.CENTER);
-
-
-
-
+        add(playingArea, BorderLayout.CENTER);
 
 
         //----Bottom Area
         bottomArea = new JPanel();
-        bottomArea.setLayout( new GridLayout(1,2,30,0));
+        bottomArea.setLayout(new GridLayout(1, 2, 30, 0));
 
         openDoorButton = new JButton("OPEN DOOR");
         openDoorButton.addActionListener(this);
-        openDoorButton.setPreferredSize(new Dimension(50,40));
+        openDoorButton.setPreferredSize(new Dimension(50, 40));
         resetButton = new JButton("RESET");
         resetButton.addActionListener(this);
 
         bottomArea.add(new JPanel());
         bottomArea.add(openDoorButton);
         bottomArea.add(resetButton);
-        add(bottomArea,BorderLayout.SOUTH);
+        add(bottomArea, BorderLayout.SOUTH);
         //----Bottom Area
 
 
-
-
-
-
-        /*particleCount = 0;
-        particles = new Particle[particleCount];
-        for ( int i=0; i<particleCount; i++ ){
-            if (i%2 == 0) {
-                particles[i] = new Particle(maxx, maxy, true);
-            }
-            else{
-                particles[i] = new Particle(maxx, maxy, false);
-            }
-            particles[i].getInfo();
-        }*/
-
-        timer = new Timer((int)(1000 * deltat), this);
+        timer = new Timer((int) (1000 * deltat), this);
         timer.start();
         pack();
         setVisible(true);
     }
 
-    public void moveAll()
-    {
-        for ( int i=0; i<particleCount; i++ ) { particles[i].move(deltat); }
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         //System.out.println("click");
-        if ( e.getSource()==timer ) { System.out.println("timer");}
-        if ( e.getSource()==openDoorButton ) {
+        if (e.getSource() == timer) {
+            //System.out.println("timer");
+        }
+        if (e.getSource() == openDoorButton) {
             System.out.println("opening door");
             //JOptionPane.showMessageDialog(null, "oop", "You have opened button", JOptionPane.INFORMATION_MESSAGE);
         }
 
-        if ( e.getSource()==resetButton ) {
+        if (e.getSource() == resetButton) {
             System.out.println("reset");
             //JOptionPane.showMessageDialog(null, "oop", "You have opened button", JOptionPane.INFORMATION_MESSAGE);
         }
@@ -140,8 +115,7 @@ public class MaxwellsDemon extends JFrame implements ActionListener{
     }
 
     @Override
-    public void paint( Graphics g )
-    {
+    public void paint(Graphics g) {
         super.paint(g);// takes too long
 /*
         g.setColor( Color.WHITE ); // just white-out the window
@@ -152,83 +126,6 @@ public class MaxwellsDemon extends JFrame implements ActionListener{
         //for ( int i=0; i<particleCount; i++ ) { particles[i].drawMe(g); }
 
     }
-
-
-
-    public class Particle
-    {
-        double x,y;
-        double vx,vy;
-        double oldx,oldy;
-        double maxx,maxy;
-        boolean isSlow;
-        double velocitycm;
-        double velocitypx;
-        double resolution = (double) (Toolkit.getDefaultToolkit().getScreenResolution() / 2.54);
-
-        public Particle(double maxx, double maxy, boolean isSlow)
-        {
-            this.maxx = maxx;
-            this.maxy = maxy;
-
-            x = Math.random() * maxx;
-            y = Math.random() * maxy;
-            this.isSlow = isSlow;
-
-            double angle = Math.random()*360;
-
-            if(isSlow == true){
-                velocitycm = Math.random()*2+2;
-            }
-
-            else{
-                velocitycm = Math.random()*2+4;
-            }
-
-            velocitypx = velocitycm * resolution;
-            vx = velocitypx * Math.cos(velocitypx);
-            vy = velocitypx * Math.sin(velocitypx);
-        }
-
-        public void move( double deltat )
-        {
-            oldx = x; oldy = y;
-            x += vx * deltat;
-            y += vy * deltat;
-            stayOnScreen();
-        }
-
-        public void stayOnScreen()
-        {
-            if ( x<0 ) { vx *= -1; }
-            if ( y<0 ) { vy *= -1; }
-            if ( x>maxx ) { vx *= -1; }
-            if ( y>maxy ) { vy *= -1; }
-        }
-
-        public void drawMe( Graphics g )
-        {
-            //g.setColor( Color.WHITE ); // erase the old one
-            //g.fillOval( (int)(oldx-2), (int)(oldy-2), 5, 5 );
-
-            if(isSlow){
-                g.setColor( Color.BLUE );
-            }
-            else{
-                g.setColor(Color.RED);
-            }
-
-            g.fillOval( (int)(x-2), (int)(y-2), 5, 5 );
-        }
-
-        public void getInfo(){
-            System.out.println("Particle Info (in cm):");
-            System.out.println("x: " + x/resolution);
-            System.out.println("y: " + y/resolution);
-            System.out.println("vx: " + vx/resolution);
-            System.out.println("vy: " + vy/resolution);
-            System.out.println("isSlow: " + isSlow);
-            System.out.println("Velocity: " + velocitycm);
-        }
-    }
 }
+
+
